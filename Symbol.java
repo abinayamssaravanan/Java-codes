a364b90fb2c548440a50d31777df562a4d26a3e7
abinayamssaravanan <162623405+abinayamssaravanan@users.noreply.github.com>
2025-08-10 23:02:24 +0530

    Determine sign of the input using conditional constructs.

---
+++ Symbol.java
@@ -0,0 +1,25 @@
+Write a  program to determine the sign of the input using conditional constructs.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Symbol {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0){
+           System .out.println("Positive");
+        }
+        else if(n<0){
+            System .out.println("Negative");
+        }
+        else{
+             System.out.println("Zero");
+        }
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
