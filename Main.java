3354038fb0058095a446dfe610bc7372d3ce5fb2
abinayamssaravanan <162623405+abinayamssaravanan@users.noreply.github.com>
2025-08-10 22:59:20 +0530

    Create Main.java
    Write a program to check the number is odd or even using conditional construct

---
+++ Main.java
@@ -0,0 +1,23 @@
+Write a C program to check the number is odd or even using conditional construct
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+            
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
